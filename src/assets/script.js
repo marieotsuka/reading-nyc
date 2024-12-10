d4f64fa1cb1afc73a445b39bbc90e47617e50525
Marie Otsuka <motsuka.gd@gmail.com>
2024-12-10 09:11:23 -0500

    updated content and styles (first half)

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,17 +1,36 @@
 console.log('s');
 
 //assign random variable values
-let txtimgs = document.querySelectorAll('.textimg');
-txtimgs.forEach( el=>{
+let randomvf = document.querySelectorAll('.random');
+randomvf.forEach( el=>{
+	assignRandomVF(el);
+});
+
+function assignRandomVF(el){
 	let wght = getRandomInt(200, 900);
 	let wdth = getRandomInt(50, 200);
+	let ital = getRandomInt(0,1);
 
 	el.style.setProperty('--wght', wght);
 	el.style.setProperty('--wdth', wdth);
+	if( ital == 1){
+		el.classList.add('italic');
+	}else{
+		if( el.classList.contains('italic') ){
+			el.classList.remove('italic');
+		}
+	}
+}
+function getRandomInt(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let textimgs = document.querySelectorAll('.textimg .random');
+textimgs.forEach( t=>{
+	t.addEventListener('click', ()=>{
+		assignRandomVF(t);
+	});
 });
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
